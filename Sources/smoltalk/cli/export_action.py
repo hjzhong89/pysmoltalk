b06fc75e159b160ab67edfa9c69fb2b1117e5bc3
Jaime Zhong <hjaimezhong@gmail.com>
2025-01-25 08:56:59 -0600

    fix: ignore pycache and fix export

--- Sources/smoltalk/cli/export_action.py
+++ Sources/smoltalk/cli/export_action.py
@@ -50,7 +50,7 @@ def get_model_source() -> str:
                 inquirer.Text('pretrained_model',
                               message="Provide Model Repository Name")
             ])
-            return response["source_dir"]
+            return response["pretrained_model"]
         case _:
             return source
 
